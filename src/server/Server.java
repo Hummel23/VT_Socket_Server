04235da8f9f7401585e4c56e17177efe53639b11
Saba <nigistsaba@hotmail.com>
2016-11-23 13:10:25 +0100

    changes server to instantiate client connections outside while loop

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,6 +1,6 @@
 package server;
-import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -10,44 +10,43 @@ public class Server {
 
 	public static void main(String[] args) {
 
-		boolean isOnline = true;
-		Protocol protocol = Protocol.getInstance();
-		
-		//thread pool created handling a maximum of 5 threads/clients at a time
-		int maxNumClients = 5;
-		ExecutorService pool = Executors.newFixedThreadPool(maxNumClients);
+		ServerSocket serverSocket;
+		Socket client;
 
 		try{
 			//start server
-			ServerSocket serverSocket = new ServerSocket(SERVER_SOCKET);
+			serverSocket = new ServerSocket(SERVER_SOCKET);
 			System.out.println("Server up and running... ");
-
+			client = serverSocket.accept();
+			boolean isOnline = true;
+			Protocol protocol = Protocol.getInstance();
+			//thread pool created handling a maximum of 5 threads/clients at a time
+			int maxNumClients = 5;
+			ExecutorService pool = Executors.newFixedThreadPool(maxNumClients);
+			ClientHandler clientHandler = new ClientHandler(client, protocol);
+			
 			while(isOnline){
 
 				//creates a thread/client connection and adds it to the Thread pool at the same time,
 				//as long as the server is running
-				pool.submit(new ClientHandler(serverSocket.accept(), protocol));
+				pool.submit(clientHandler);
 			}
-			
+
 			//when server is offline shutdown the pool 	
 			pool.shutdown();
-			try {
-				while (!pool.isShutdown()) {
-					
-					//pool is waiting 500 milliseconds before terminating 
-					pool.awaitTermination(500, TimeUnit.MILLISECONDS);
-				}
-			} catch (InterruptedException e) {
-				System.out.println("Pool could not be shut down properly.");
+
+			while (!pool.isShutdown()) {
+
+				//pool is waiting 500 milliseconds before terminating 
+				pool.awaitTermination(500, TimeUnit.MILLISECONDS);
 			}
-			
+
 			//then close socket
 			serverSocket.close();
 
 
-		}catch (IOException e) {
-			isOnline = false;
-			System.out.println("Server is down.");
+		}catch (Exception e) {
+			e.printStackTrace();
 		}
 
 	}
